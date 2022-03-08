5ebee98a918e9f76fed978698cc910b8752c86a5
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-07 23:33:10 -0500

    Modified Chip8GameModel to have fields required by form.

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from .fields import Chip8FileField
 class Chip8GameModel(models.Model):
     '''Represents one chip 8 game'''
 
-    file = Chip8FileField(upload_to='games')
-    description = models.TextField(null=True,blank=True,max_length=2000)
+    file = Chip8FileField(blank=False,upload_to='games')
+    description = models.TextField(null=True,blank=False,max_length=2000)
     title = models.CharField(blank=False,null=False,default="Please add a title.",max_length=500)
-    author = models.CharField(blank=True,null=True,max_length=100)
+    author = models.CharField(blank=False,null=True,max_length=100)
