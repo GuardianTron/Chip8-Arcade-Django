57375e6994f0f1e17eca826307dba07522a20e17
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-08 00:20:20 -0500

    Added tests for Chip8GameModel and correct some errors.

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
 from .fields import Chip8FileField
 
 
@@ -6,6 +7,6 @@ class Chip8GameModel(models.Model):
     '''Represents one chip 8 game'''
 
     file = Chip8FileField(blank=False,upload_to='games')
-    description = models.TextField(null=True,blank=False,max_length=2000)
-    title = models.CharField(blank=False,null=False,default="Please add a title.",max_length=500)
-    author = models.CharField(blank=False,null=True,max_length=100)
+    description = models.TextField(null=True,blank=False,max_length=2000,validators=[MaxLengthValidator(2000)])
+    title = models.CharField(blank=False,null=False,default="Please add a title.",max_length=100)
+    author = models.CharField(blank=True,null=True,max_length=100)
--- tests/test_chip8model.py
+++ tests/test_chip8model.py
@@ -1,6 +1,51 @@
+from django.core.exceptions import ValidationError
 from chip8.models import Chip8GameModel
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class Chip8ModelTest(TestCase):
 
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.game = Chip8GameModel.objects.create(title="My Game",
+                        description="This is my game.",
+                        author="teh author",
+                        file = SimpleUploadedFile('mygame',content=b"haha")
+                    )
+
+    def test_requires_title(self):
+        self.game.title=""
+        with self.assertRaises(ValidationError):
+            self.game.full_clean()
+
+    def test_requires_description(self):
+        self.game.description = ""
+        with self.assertRaises(ValidationError):
+            self.game.full_clean()
+
+    def test_requires_file(self):
+        self.game.file = None
+        with self.assertRaises(ValidationError):
+            self.game.full_clean()
+
+    def test_does_not_require_author(self):
+        self.game.author = ''
+        self.game.clean()
+
+    def test_author_max_length(self):
+        self.game.author = "a"*101
+        with self.assertRaises(ValidationError):
+            self.game.full_clean()
+
+    def test_title_max_length(self):
+        self.game.title = "a"*101
+        with self.assertRaises(ValidationError):
+            self.game.full_clean()
+
+    def test_description_max_length(self):
+        self.game.description="a"*2001
+        with self.assertRaises(ValidationError):
+            self.game.full_clean()
+
+    def test_passes(self):
+        self.game.full_clean()
