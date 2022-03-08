9e2599bac803221a9d0b94247b47f256e2eb6785
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-07 23:10:30 -0500

    Added description, title, and author fields to model.

--- models/models.py
+++ models/models.py
@@ -6,3 +6,6 @@ class Chip8GameModel(models.Model):
     '''Represents one chip 8 game'''
 
     file = Chip8FileField(upload_to='games')
+    description = models.TextField(null=True,blank=True,max_length=2000)
+    title = models.CharField(blank=False,null=False,default="Please add a title.",max_length=500)
+    author = models.CharField(blank=True,null=True,max_length=100)
