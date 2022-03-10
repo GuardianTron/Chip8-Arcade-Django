a9ad400cf99697b14bdef54a51af4368e4e01ceb
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-09 20:11:12 -0500

    Initial defintion of keyboard code configuration model.

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxLengthValidator
+from django.core.validators import MaxLengthValidator,MaxValueValidator
 from .fields import Chip8FileField
 from .validators import MaxFilesizeValidator
 
@@ -16,3 +16,18 @@ class Chip8GameModel(models.Model):
     description = models.TextField(null=True,blank=False,max_length=2000,validators=[MaxLengthValidator(2000)])
     title = models.CharField(blank=False,null=False,default="Please add a title.",max_length=100)
     author = models.CharField(blank=True,null=True,max_length=100)
+
+
+class KeyConfig(models.Model):
+
+    '''Links chip 8 keys to physical keyboard keys using Javascript KeyboardEvent.code'''
+
+    class Meta:
+        verbose_name = "Key Configuration"
+        verbose_name_plural = "Key Configuration"
+        constraints = [models.UniqueConstraint(fields=["game_id","keyboard_code"],name="unique_keyboard_key")]
+    
+    game_id = models.ForeignKey(Chip8FileField,on_delete=models.CASCADE,related_name='keys')
+    chip8_key = models.PositiveSmallIntegerField(verbose_name="Chip 8 Key",validators=[MaxValueValidator(15)])
+    keyboard_code = models.CharField(verbose_name="Javascript Keyboard Code",max_length=20)
+
