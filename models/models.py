e174cdb30ceb6f136ee0c4b1786ff29593fb74a3
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-15 00:42:40 -0400

    Made both configurations classes inherit from a common base class.

--- models/models.py
+++ models/models.py
@@ -19,7 +19,18 @@ class Chip8GameModel(models.Model):
     author = models.CharField(blank=True,null=True,max_length=100)
 
 
-class KeyConfigModel(models.Model):
+class AbstractConfigModel(models.Model):
+    '''Acts as the base model for all configuration classes.'''
+    
+    class Meta:
+        abstract=True
+
+
+    chip8_key = models.PositiveSmallIntegerField(verbose_name="Chip 8 Key",validators=[MaxValueValidator(15)])
+
+
+
+class KeyConfigModel(AbstractConfigModel):
 
     '''Links chip 8 keys to physical keyboard keys using Javascript KeyboardEvent.code'''
 
@@ -29,11 +40,10 @@ class KeyConfigModel(models.Model):
         constraints = [models.UniqueConstraint(fields=["game_id","keyboard_code"],name="unique_keyboard_key")]
     
     game_id = models.ForeignKey(Chip8GameModel,on_delete=models.CASCADE,related_name='keys')
-    chip8_key = models.PositiveSmallIntegerField(verbose_name="Chip 8 Key",validators=[MaxValueValidator(15)])
     keyboard_code = models.CharField(verbose_name="Javascript Keyboard Code",max_length=20,validators=[RegexValidator('^[A-Z][A-Za-z]*[0-9]{0,2}$')])
 
 
-class ButtonConfigModel(models.Model):
+class ButtonConfigModel(AbstractConfigModel):
 
     '''Links chip 8 keys to button icons for touch controls'''
 
@@ -55,6 +65,6 @@ class ButtonConfigModel(models.Model):
         ('button_z','Z Button')
     ]
 
-    game_id = models.ForeignKey(Chip8GameModel,on_delete=models.CASCADE,related_name="buttons")
-    chip8_key = models.PositiveBigIntegerField(verbose_name="Chip 8 Key",validators=[MaxValueValidator(15)])
+
+    game_id = models.ForeignKey(Chip8GameModel,on_delete=models.CASCADE,related_name='buttons')
     button_id = models.CharField(verbose_name="Button Id",max_length=10, choices=BUTTON_CHOICES, default='dir_left')
