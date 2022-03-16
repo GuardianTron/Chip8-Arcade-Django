3547154b001191f488c9266e5b690cd124f62aff
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-16 13:12:08 -0400

    Added the button configuration and key configuration as inlines for the chip 8 game admin interface.

--- admin.py
+++ admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Chip8GameModel
+from .models import Chip8GameModel,KeyConfigModel,ButtonConfigModel
 # Register your models here.
-admin.site.register(Chip8GameModel)
+class KeyConfigInline(admin.TabularInline):
+    model = KeyConfigModel
+
+class ButtonConfigInline(admin.TabularInline):
+    model = ButtonConfigModel
+
+@admin.register(Chip8GameModel)
+class Chip8GameAdmin(admin.ModelAdmin):
+    inlines=[KeyConfigInline,ButtonConfigInline]
