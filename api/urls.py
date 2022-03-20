91b09b5556e3d5220cd9a5f073167bd67d2ecaf1
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-19 23:56:27 -0400

    Added named routes to the json api

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import GameListView,GameView
 
 urlpatterns = [
-    path('',GameListView.as_view()),
-    path('<int:pk>',GameView.as_view()),
+    path('',GameListView.as_view(),name="list_json"),
+    path('<int:pk>',GameView.as_view(),name="game_json"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
