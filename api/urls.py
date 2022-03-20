344d111e63fa9dcc5ca666d33f3da971c7048c64
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-19 23:46:03 -0400

    Added the ability to access the json and api via suffixes.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import GameListView,GameView
 
 urlpatterns = [
     path('',GameListView.as_view()),
     path('<int:pk>',GameView.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
