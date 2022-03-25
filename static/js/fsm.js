404255270f79cd04c0607d2c8af4f62e6e6c42d4
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-25 15:40:58 -0400

    Added in basic infrastructure do generate a menu from retrieved JSON.  Note: event listeners have not yet been coded for the menu, simply building the menu and managing it within the wider application.

--- static/js/fsm.js
+++ static/js/fsm.js
@@ -3,6 +3,9 @@
 export class AbstractState{
 
     construct(stateMachine){
+        if(!stateMachine instanceof StateMachine){
+            throw new TypeError('Instance of class StateMachine required.')
+        }
         this._fsm = stateMachine
     }
 
@@ -10,7 +13,7 @@ export class AbstractState{
         return this._fsm;
     }
 
-    enter = () =>{
+    enter = async () =>{
         throw new Error("Callers must override base implementation.");
     }
 
@@ -53,6 +56,105 @@ export class StateMachine{
         this._states.get(stateLabel).enter(argumentsObject);
     }
 
+
+
+
+}
+
+
+class ApplicationState extends AbstractState{
+
+    construct(stateMachine,containingDOMElement){
+        super(stateMachine);
+        this._containingDOMElement = containingDOMElement;
+    }
+
+    get container(){
+        return this._containingDOMElement;
+    }
+}
+
+class MenuState extends ApplicationState{
+    contsruct(stateMachine,containingDOMElement,menuSourceUrl){
+        super(stateMachine,containingDOMElement);
+        this._url = menuSourceUrl;
+        this._menuBuilder = null;
+    }
+
+    enter = async () =>{
+        //fetch and build menu if not already cached
+        if(!this._menuBuilder){
+            this._menuBuilder = await buildMenu(this._url,SelectMenuBuilder());
+        }
+        this._menuBuilder.activateEventListeners();
+        this.container.appendChild(this._menuBuilder.getMenu());
+
+    }
+
+    exit = ()=>{
+        this.container.removeChild(this._menuBuilder.getMenu());
+        this._menuBuilder.deactivateEventListeners();
+    }
+}
+
+async function buildMenu(sourceURL,menuBuilder){
+    let response = await fetch(sourceURL);
+    let menuData = await response.json();
+    Object.keys(menuData).forEach( key => {
+        menuBuilder.addOption(key,menuData[key]);
+
+    });
+
+    return menuBuilder;
+        
+}
+
+class SelectMenuBuilder{
+
+    construct(){
+        this._menu = document.createElement('select');
+        this._listeners = {};
+        this._areListenersActivated = false;
+    }
+
+    addOption = (value,text) =>{
+        option = document.createElement('option');
+        option.setAttribute('value',value);
+        option.appendChild(document.createTextNode('text'));
+        this._menu.appendChild(option);
+        this._menu.setAttribute('size',this._menu.children.length);
+        
+    }
+
+    registerEventListener = (listenerType,func)=>{
+         if(!listenerType in this._listeners){
+             this._listeners[listenerType] = [];
+         }   
+         this._listeners[listenerType] = func;
+         
+    }
+
+    activateEventListeners = () =>{
+        if(!this._areListenersActivated){
+            Object.key(this._listeners).forEach( key =>{
+                this._menu.addEventListener(key,this._listeners[key]);
+            });
+            this._areListenersActivated = true;
+        }
+    }
+
+    deactivateEventListeners = () =>{
+        if(this._areListenersActivated){
+            Object.keys(this._listeners).forEach( key =>{
+                this._menu.removeEventListener(key,this)
+            });
+            this._areListenersActivated = false;
+        }
+    }
+
+    getMenu = () =>{
+        return this._menu;
+    }
 
 
 }
