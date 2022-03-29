346d3e2a705add4a6fafbacec00dbf9d3d172e70
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-03-29 00:37:06 -0400

    Begun creating interface for emulator.

--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
 from django.urls import path,include
 
 
+from .views import player
+
 app_name = "chip8"
 
 urlpatterns = [
+    path('',player,name='player'),
     path('api/',include('chip8.api.urls')),
+
 ]
--- views.py
+++ views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def player(request):
+    return render(request,'chip8/player.html')
+
 
