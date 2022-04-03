2d05847bf07c35386ed87b5e221a692ef6fadc52
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-04-02 23:25:10 -0400

    Begun work on showing the description screen for the game.

---
+++ static/js/app_states/description_state.js
@@ -0,0 +1,120 @@
+import { ApplicationState } from "../fsm.js";
+
+export class DescriptionState extends ApplicationState{
+
+    constructor(stateMachine,containingDOMElement, url){
+        super(stateMachine,containingDOMElement);
+        this._games = new GameModel(url);
+        this._descriptionBuilder = new DescriptionBuilder();
+        this.container.appendChild(this._descriptionBuilder.element);
+       }
+
+    enter = async ({game_id = null}) => {
+        const gameData = await this._games.fetch(game_id);
+        console.log(gameData);
+        this._descriptionBuilder.titleText = gameData.title;
+        this._descriptionBuilder.descriptionText = gameData.description;
+        
+        
+    }
+    
+}
+
+class GameModel{
+
+    constructor( base_url ){
+        this._base_url = base_url;
+        this._gameCache = new Map();
+    }
+
+    fetch = async (game_id)=>{
+        if(this._gameCache.has(game_id)){
+            return this._gameCache.get(game_id);
+        }
+        const gameResponse = await fetch(`${this._base_url}${game_id}?format=json`);
+        const gameData = await gameResponse.json();
+        this._gameCache.set(game_id,gameData);
+        return gameData;
+
+    }
+
+
+
+
+
+}
+
+class ElementBuilder{
+     constructor(elementType,{id = null, htmlClass = null}={}){
+         this._element = document.createElement(elementType);
+         if(id){
+             this._element.setAttribute('id',id);
+         }
+         if(htmlClass){
+             this._element.setAttribute('class',htmlClass)
+         }
+     }
+
+     get element(){
+         return this._element;
+     }
+
+     set textContent(content){
+         this.removeChildren();
+         this.element.appendChild(document.createTextNode(content));
+     }
+
+     removeChildren = ()=>{
+         while(this._element.firstChild){
+             this._element.removeChild(this._element.firstChild);
+         }
+     }
+
+
+
+
+
+
+}
+
+class ParagraphContainerBuilder extends ElementBuilder{
+
+    set textContent(content){
+        this.removeChildren();
+        const paragraphContent = content.split(/\r?\n/);
+
+        paragraphContent.forEach(element => {
+            let paragraph = document.createElement('p');
+            paragraph.appendChild(document.createTextNode(content));
+            this.element.appendChild(paragraph);
+
+        });
+
+    }
+}
+
+
+
+class DescriptionBuilder extends ElementBuilder{
+
+    constructor({id=null,htmlClass=null}={}){
+        super('div',{id:id,htmlClass:htmlClass});
+        this._titleElement = new ElementBuilder('h1');
+        this._descriptionElement = new ParagraphContainerBuilder('div');
+
+        this.element.appendChild(this._titleElement.element);
+        this.element.appendChild(this._descriptionElement.element);
+    }
+
+    set titleText(content){
+        this._titleElement.textContent = content;
+    }
+
+    set descriptionText(content){
+        this._descriptionElement.textContent = content;
+    }
+
+
+
+
+}
--- static/js/game_player.js
+++ static/js/game_player.js
@@ -1,13 +1,17 @@
 "use strict";
 import { StateMachine } from "./fsm.js";
 import { MenuState } from "./app_states/menu_state.js";
+import { DescriptionState } from "./app_states/description_state.js";
 
 const fsm = new StateMachine();
-const menu = new MenuState(fsm,document.getElementById('player_screen'),'api/');
+const containerElement = document.getElementById('player_screen');
+const menu = new MenuState(fsm,containerElement,'api/');
+const description = new DescriptionState(fsm,containerElement,'api/');
 //set up controls for menu
 menu.addButton(document.getElementById('d_pad'));
 menu.addButton(document.getElementById('start_select'));
 //add states to state manchine
 fsm.addState('menu_state',menu);
+fsm.addState('description_state',description);
 //start in the menu state
 fsm.changeState('menu_state');
