ff15d06a4188ee8def4b252d3575fca935fb35de
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-04-09 23:44:41 -0400

    Added start button to description state.  Cleaned up code for getting button elements.

--- static/js/game_player.js
+++ static/js/game_player.js
@@ -7,9 +7,18 @@ const fsm = new StateMachine();
 const containerElement = document.getElementById('player_screen');
 const menu = new MenuState(fsm,containerElement,'api/');
 const description = new DescriptionState(fsm,containerElement,'api/');
+
+const startSelect = document.getElementById('start_select');
+const dPad = document.getElementById('d_pad');
+const actionButtons = document.getElementById('buttons');
+
 //set up controls for menu
-menu.addButton(document.getElementById('d_pad'));
-menu.addButton(document.getElementById('start_select'));
+menu.addButton(dPad);
+menu.addButton(startSelect);
+
+//set up conrols for description
+description.addButton(startSelect);
+
 //add states to state manchine
 fsm.addState('menu_state',menu);
 fsm.addState('description_state',description);
