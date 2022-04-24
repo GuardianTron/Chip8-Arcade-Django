3aa17c8c1982f42f3d6b9398df4b1e2d8b80ee2b
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-04-23 22:08:26 -0400

    Added (untested) code to load and start game roms.

--- static/js/app_states/game_state.js
+++ static/js/app_states/game_state.js
@@ -1,20 +1,23 @@
 import { StateMachine, ApplicationState } from "../fsm.js";
 import { Chip8Emulator } from "../Chip-8-Emulator/chip8emulator.js";
+import { loadGame } from "../Chip-8-Emulator/loaders.js";
 
 
 export class GameState extends ApplicationState{
 
-    constructor(containingDOMElement){
+    constructor(containingDOMElement, chip8FontURL, superChipFontURL){
         super(containingDOMElement);
         this._canvasElement = document.createElement('canvas');
         this._canvasElement.id = "game_canvas";
         this._emulator = new Chip8Emulator(this._canvasElement);
+        this._chip8FontURL = chip8FontURL;
+        this._superChipFontURL = superChipFontURL
     }
 
     enter = async ({gameData})=>{
         console.log(gameData);
         this.mapKeys(gameData.keys);
-        this.container.appendChild(this._canvasElement);
+        loadGame(this._emulator,this._chip8FontURL,this._superChipFontURL,gameData.file,fetchAndTranscodeGameRom);
 
     }
 
@@ -25,6 +28,29 @@ export class GameState extends ApplicationState{
       });
     }
 
+
+}
+
+
+async function fetchAndTranscodeGameRom(romURL){
+    const response = await fetch(romURL);
+    if(!response.ok){
+        throw new Error(`Unable to load rom ${romURL}`);
+    }
+    const hexString = await response.text();
+    return hexStringToBinBuffer(hexString);
+}
+
+function hexStringToBinBuffer(hexString){
+    //note: hex string is twice the size of actual data
+    const buffer = new Uint8Array(hexString.length/2);
+    //every two characters represents a byte.
+    for(let i=0; i < hexString.length; i+=2){
+        let byteString = hexString.slice(i,i+1);
+        buffer[i/2] = parseInt(byteString,16);
+        
+    }
+    return buffer
 
 }
 
